68d58b4cc033b2c55a1e252ff3e89b2f049ab145
Thomas Clover <thomasc@sitem.co.th>
2026-02-22 11:59:28 +0700

    Add file server handler for index.html

--- main.go
+++ main.go
@@ -6,12 +6,15 @@ import (
 )
 
 func main() {
-	// 1. Create a new ServeMux
 	mux := http.NewServeMux()
 
-	// 2. Register a handler function !!NOT YET!!
+	// Create a file server handler pointer to a root directory
+	fileServer := http.FileServer(http.Dir("."))
 
-	// 3. Listen and serve on port 8080 using the custom mux
+	// Register the file server to the root path
+	mux.Handle("/", fileServer)
+
+	// Listen and serve on port 8080 using the custom mux
 	fmt.Println("Server starting on :8080...")
 	err := http.ListenAndServe(":8080", mux)
 	if err != nil {
